51f0bda0e195b36703b9b7394f3552d9050060b6
Cesar <cesar@celke.com.br>
2024-05-05 11:08:08 -0300

    Como criar a base da API com CRUD completo

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# tutorial-como-criar-crud-api-laravel11
-Como criar o CRUD completo na API com Laravel 11
+## Requisitos
+
+* PHP 8.2 ou superior
+* MySQL 8 ou superior
+* Composer
+
+## Como rodar o projeto baixado
+
+Duplicar o arquivo ".env.example" e renomear para ".env".<br>
+Alterar no arquivo .env as credenciais do banco de dados<br>
+
+Instalar as dependências do PHP
+```
+composer install
+```
+
+Gerar a chave no arquivo .env
+```
+php artisan key:generate
+```
+
+Executar as migration
+```
+php artisan migrate
+```
+
+Executar as seed
+```
+php artisan db:seed
+```
+
+Iniciar o projeto criado com Laravel
+```
+php artisan serve
+```
+
+Para acessar a API, é recomendado utilizar o Insomnia para simular requisições à API.
+```
+http://127.0.0.1:8000/api/users
+```
+
+
+## Sequencia para criar o projeto
+Como criar o projeto com Laravel
+```
+composer create-project laravel/laravel .
+```
+
+Alterar no arquivo .env as credenciais do banco de dados<br>
+
+Como criar o arquivo de rotas para API no Laravel 11
+```
+php artisan install:api
+```
+
+Como iniciar o projeto criado com Laravel
+```
+php artisan serve
+```
+
+Para acessar a API, é recomendado utilizar o Insomnia para simular requisições à API.
+```
+http://127.0.0.1:8000/api/users
+```
+
+Como criar seed com artisan no Laravel 11
+```
+php artisan make:seeder NomeDaSeeder
+php artisan make:seeder UserSeeder
+```
+
+Como executar as seed com artisan no Laravel 11
+```
+php artisan db:seed
+```
+
+Como criar a Controller com artisan no Laravel 11
+```
+php artisan make:model NomeDaController
+php artisan make:controller UserController
+```
+
+Como criar o Request com artisan no Laravel 11
+```
+php artisan make:request NomeDoRequest
+php artisan make:request UserRequest
+```
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+
+    /**
+     * Retorna uma lista paginada de usuários.
+     *
+     * Este método recupera uma lista paginada de usuários do banco de dados
+     * e a retorna como uma resposta JSON.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        // Recupera os usuários do banco de dados, ordenados pelo id em ordem decrescente, paginados
+        $users = User::orderBy('id', 'DESC')->paginate(2);
+
+        // Retorna os usuários recuperados como uma resposta JSON
+        return response()->json([
+            'status' => true,
+            'users' => $users,
+        ], 200);
+    }
+
+    /**
+     * Exibe os detalhes de um usuário específico.
+     *
+     * Este método retorna os detalhes de um usuário específico em formato JSON.
+     *
+     * @param  \App\Models\User  $user O objeto do usuário a ser exibido
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(User $user): JsonResponse
+    {
+        // Retorna os detalhes do usuário em formato JSON
+        return response()->json([
+            'status' => true,
+            'user' => $user,
+        ], 200);
+    }
+
+    /**
+     * Cria novo usuário com os dados fornecidos na requisição.
+     * 
+     * @param  \App\Http\Requests\UserRequest  $request O objeto de requisição contendo os dados do usuário a ser criado.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(UserRequest $request): JsonResponse
+    {
+        // Iniciar a transação
+        DB::beginTransaction();
+
+        try {
+
+            // Cadastrar usuário no banco de dados
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            // Operação é concluída com êxito
+            DB::commit();
+
+            // Retorna os dados do usuário criado e uma mensagem de sucesso com status 201
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário cadastrado com sucesso!",
+            ], 201);
+        } catch (Exception $e) {
+
+            // Operação não é concluída com êxito
+            DB::rollBack();
+
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não cadastrado!",
+            ], 400);
+        }
+    }
+
+    /**
+     * Atualizar os dados de um usuário existente com base nos dados fornecidos na requisição.
+     * 
+     * @param  \App\Http\Requests\UserRequest  $request O objeto de requisição contendo os dados do usuário a ser atualizado.
+     * @param  \App\Models\User  $user O usuário a ser atualizado.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(UserRequest $request, User $user): JsonResponse
+    {
+
+        // Iniciar a transação
+        DB::beginTransaction();
+
+        try {
+
+            // Editar o registro no banco de dados
+            $user->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            // Operação é concluída com êxito
+            DB::commit();
+
+            // Retorna os dados do usuário editado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário editado com sucesso!",
+            ], 200);
+        } catch (Exception $e) {
+
+            // Operação não é concluída com êxito
+            DB::rollBack();
+
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não editado!",
+            ], 400);
+        }
+    }
+
+    /**
+     * Excluir usuário no banco de dados.
+     * 
+     * @param  \App\Models\User  $user O usuário a ser excluído.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        try {
+
+            // Apagar o registro no banco de dados
+            $user->delete();
+
+            // Retorna os dados do usuário apagado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário apagado com sucesso!",
+            ], 200);
+
+
+        } catch (Exception $e) {
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não apagado!",
+            ], 400);
+        }
+    }
+}
